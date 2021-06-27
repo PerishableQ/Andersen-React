f62f039f551166ebc34bddbaa12306d37bad9d3b
PerishableQ <redemption.yep@gmail.com>
2021-06-27 17:07:00 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 //Import JS
-// import Header from "./components/Header/Header";
-// import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
-// import HomePage from "./components/HomePage/HomePage";
-// import SearchResult from "./components/SearchResult/SearchResult";
-// =======
-// import SignIn from "./components/SignIn/SignIn";
-// import SignUp from "./components/SignUp/SignUp";
-// import CardInfo from "./components/CardInfo/CardInfo";
-// import Favorites from "./components/Favorites/Favorites";
-// import SearchHistory from "./components/SearchHistory/SearchHistory";
+import HomePage from "./components/HomePage/HomePage";
+import SearchResult from "./components/SearchResult/SearchResult";
+
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
+import CardInfo from "./components/CardInfo/CardInfo";
+import Favorites from "./components/Favorites/Favorites";
+import SearchHistory from "./components/SearchHistory/SearchHistory";
 
 
 // Import CSS
@@ -26,14 +26,14 @@ function App() {
 			<main className="page">
 				{/* Сюда кладем все компоненты кроме хедера и футера */}
 
-// 				<HomePage />
-// 				<SearchResult />
+				<HomePage />
+			    <SearchResult />
 
-// 				{/* <SignIn /> */}
-// 				{/* <SignUp /> */}
-// 				{/* <CardInfo /> */}
-// 				{/* <Favorites /> */}
-// 				<SearchHistory />
+ 				{/* <SignIn /> */}
+ 				{/* <SignUp /> */}
+ 				{/* <CardInfo /> */}
+ 				{/* <Favorites /> */}
+ 				<SearchHistory />
 
 			</main>
 
