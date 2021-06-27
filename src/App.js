27fdf1a4f5250dfc1f1a389158119905e22c2b10
PerishableQ <redemption.yep@gmail.com>
2021-06-27 14:44:32 +0300

    signin page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import SignIn from './components/SIgnIn/SignIn';
 
 function App() {
   return (
     <div className="App">
-      SearchMovies
+      <SignIn />
     </div>
   );
 }
