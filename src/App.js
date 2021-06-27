040d6c746ae0f94a0985fd56bd7cb1fb5ecf64aa
PerishableQ <redemption.yep@gmail.com>
2021-06-27 20:13:28 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 //Import JS
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
-import HomePage from "./components/HomePage/HomePage";
-import SearchResult from "./components/SearchResult/SearchResult";
-
-import SignIn from "./components/SignIn/SignIn";
-import SignUp from "./components/SignUp/SignUp";
-import CardInfo from "./components/CardInfo/CardInfo";
-import Favorites from "./components/Favorites/Favorites";
-import SearchHistory from "./components/SearchHistory/SearchHistory";
+import AppRouter from "./Router";
 
 // Import CSS
 import "./Reset.css";
@@ -26,16 +19,7 @@ function App() {
 
 				<main className="page">
 					{/* Сюда кладем все компоненты кроме хедера и футера */}
-
-					<Switch>
-						<Route path="/" exact component={HomePage}></Route>
-						<Route path="/favorites" component={Favorites}></Route>
-						<Route path="/searchhistory" component={SearchHistory}></Route>
-						<Route path="/searchresult" component={SearchResult}></Route>
-						<Route path="/signin" component={SignIn}></Route>
-						<Route path="/signup" component={SignUp}></Route>
-						<Route path="/cardinfo" component={CardInfo}></Route>
-					</Switch>
+					<AppRouter />
 				</main>
 
 				<Footer />
--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
 //Import JS
 // import Header from "./components/Header/Header";
 // import Footer from "./components/Footer/Footer";
@@ -15,23 +14,20 @@ import CardInfo from "./components/CardInfo/CardInfo";
 import Favorites from "./components/Favorites/Favorites";
 import SearchHistory from "./components/SearchHistory/SearchHistory";
 
-
 // Import CSS
 import "./Reset.css";
 import "./App.css";
 
-function AppRouter() {
-    <Router>
-        <Switch>
-            <Route path='/' exact component={HomePage}></Route>
-            <Route path='/favorites' exact component={Favorites}></Route>
-            <Route path='/searchhistory' exact component={SearchHistory}></Route>
-            <Route path='/searchresult' exact component={SearchResult}></Route>
-            <Route path='/signin' exact component={SignIn}></Route>
-            <Route path='/signup' exact component={SignUp}></Route>
-            <Route path='/cardinfo' exact component={CardInfo}></Route>
-        </Switch>
-    </Router>
-}
+const AppRouter = () => (
+	<Switch>
+		<Route path="/" exact component={HomePage}></Route>
+		<Route path="/favorites" exact component={Favorites}></Route>
+		<Route path="/searchhistory" exact component={SearchHistory}></Route>
+		<Route path="/searchresult" exact component={SearchResult}></Route>
+		<Route path="/signin" exact component={SignIn}></Route>
+		<Route path="/signup" exact component={SignUp}></Route>
+		<Route path="/cardinfo" exact component={CardInfo}></Route>
+	</Switch>
+);
 
 export default AppRouter;
