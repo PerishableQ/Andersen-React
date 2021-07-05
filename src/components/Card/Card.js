29e22e007550afe0cbfd26b8e65032aef1a0ccdb
PerishableQ <redemption.yep@gmail.com>
2021-07-05 17:30:51 +0300

    favorites icon renders only when signed in

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,11 @@
-import "./Card.scss";
+import { useStoreAuth } from "../../hooks/useStoreAuth";
+
 import FavoritesIcon from "./FavoriteIcon/FavoritesIcon";
+import "./Card.scss";
 
 function Card(props) {
+	const isLoggedIn = useStoreAuth();
+
 	return (
 		<div className="info__card-item card-item" id={props.id}>
 			<div className="card-item__img">
@@ -11,7 +15,7 @@ function Card(props) {
 				<h3 className="card-item__title">{props.title}</h3>
 				<p className="card-item__year">{props.year}</p>
 			</div>
-            <FavoritesIcon card={props.card} isFavorite={props.isFavorite} />
+			{isLoggedIn ? <FavoritesIcon card={props.card} isFavorite={props.isFavorite} /> : null}
 		</div>
 	);
 }
