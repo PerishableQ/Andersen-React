33bd036522486bc91aebfbc81622afd0b6d08393
Artem Sedykh <sedixtema@icloud.com>
2021-07-06 07:32:06 +0300

    Добавлен переход на главную страницу после клика на кнопку Выход

--- src/components/SignOutBtn/SignOutBtn.js
+++ src/components/SignOutBtn/SignOutBtn.js
@@ -1,14 +1,17 @@
 import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 import { logout } from "../../redux/reducers/authSlice";
 
 export function SignOutBtn(props) {
 	const dispatch = useDispatch();
+	let history = useHistory();
 
 	const signOut = () => {
 		console.log("!");
 		localStorage.setItem("currentUser", "");
 		dispatch(logout());
+		history.push("/");
 	};
 
 	return (
